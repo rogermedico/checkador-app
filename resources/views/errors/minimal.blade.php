7465cd3c3c834c0bdf754c9f18a492c7fde822a1
roger <roger.medico@gmail.com>
2022-01-23 12:55:46 +0100

    storage symlink and errors fix

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>NRS - @yield('title')</title>
+    <title>{{env('APP_NAME')}} - @yield('title')</title>
     <link rel="icon" type="image/png" href="{{asset('storage/favicon/favicon.ico')}}">
     <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
 </head>
@@ -11,7 +11,7 @@
 <main class="container mt-5 pt-5 d-flex flex-column justify-content-center align-items-center">
     <div>
         <div class="offset-4 col-4 mb-3">
-            <img class="img-fluid" src="{{asset('storage/logos/logo.png')}}" alt="NRS Logo">
+            <img class="img-fluid" src="{{asset('storage/logos/logo.png')}}" alt="{{env('APP_NAME')}} Logo">
         </div>
         <div class="text-center text-lowercase">
             <h2 class="serif">
