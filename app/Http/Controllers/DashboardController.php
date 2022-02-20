8d2d62726618d88ae4706d630dafb39f53f1162a
roger <roger.medico@gmail.com>
2022-02-20 21:35:42 +0100

    dashboard calendar

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,15 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use App\Models\EventType;
+use App\Services\EventService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(EventService $eventService)
     {
+        $user = auth()->user();
 
+        $currentMonth = Carbon::createFromDate(
+            $year ?? now()->year,
+            $month ?? now()->month,
+            1
+        );
 
-        return view('dashboard.index');
+        $previousMonth = clone $currentMonth;
+        $previousMonth->subMonth();
+
+        $nextMonth = clone $currentMonth;
+        $nextMonth->addMonth();
+
+        $events = Event::with('eventType')
+            ->where('user_id', $user->id ?? auth()->user()->id)
+            ->whereMonth('date', $currentMonth->month )
+            ->whereYear('date', $currentMonth->year)
+            ->get();
+
+        $timeSpentWorkingByDay = $eventService->TimeSpentWorkingByDay($events);
+
+        return view('dashboard.index', compact(
+            'user',
+            'timeSpentWorkingByDay',
+            'currentMonth',
+            'previousMonth',
+            'nextMonth',
+        ));
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,18 +4,21 @@
         <h1 class="offset-lg-2 col-lg-4 serif">{{__('dashboard')}}</h1>
         <x-messages :message="$message ?? null" />
         <x-errors/>
-        <div class="d-lg-flex flex-lg-row flex-gap">
-            <section class="col-lg-4 px-lg-1">
-                <h2 class="text-lg-center">{{__('calendar')}}</h2>
+        <div class="d-xl-flex flex-xl-row flex-gap">
+            <section class="col-xl-4 px-xl-1">
+                <h2 class="text-xl-center">{{__('calendar')}}</h2>
+                <div>
+                    @include('dashboard.partials.calendar')
+                </div>
             </section>
-            <section class="col-lg-4 px-lg-1">
-                <h2 class="text-lg-center">{{__('register event')}}</h2>
+            <section class="col-xl-4 px-xl-1">
+                <h2 class="text-xl-center">{{__('register event')}}</h2>
                 <div>
                     @include('event.partials.create_form')
                 </div>
             </section>
-            <section class="col-lg-4 px-lg-1">
-                <h2 class="text-lg-center">{{__('actual week')}}</h2>
+            <section class="col-xl-4 px-xl-1">
+                <h2 class="text-xl-center">{{__('actual week')}}</h2>
             </section>
         </div>
 @endsection
